1b81db64c7667f7d95e3a2a81c52c2b4b3f1c8de
Mubarak Hossain <mubarakkha355@gmail.com>
2022-10-08 16:55:07 +0600

    Named Routes

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::get('/about', function () {
+Route::get('/about-page', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/service', function () {
     return view('service');
-});
+})->name('service');
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
